c1ed705acc2df579fba9658effa912508ea4eb4d
Sunny <76804876+sunnyh2024@users.noreply.github.com>
2025-02-18 20:59:19 -0500

    add gh-pages

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -40,7 +40,7 @@ export default function Landing() {
             <motion.div whileHover={{scale: 1.1}} onClick={() => {
               navigator.clipboard.writeText(email);
               alert(`Copied ${email} to clipboard!`);
-            }}><img src={`/email.svg`} alt={`email icon`} /></motion.div>
+            }}><img src={`/email2.svg`} alt={`email icon`} /></motion.div>
           </div>
         </div>
         <hr className="line" />
