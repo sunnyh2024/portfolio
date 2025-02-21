fb9a387a7f957b1fbf8bf78fc42b437ffd961b1d
Sunny <76804876+sunnyh2024@users.noreply.github.com>
2025-02-20 22:08:56 -0500

    update svgs

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,9 +3,9 @@ import "./footer.css";
 
 export default function Footer() {
   const fontLinks = [
-    ["https://fonts.google.com/specimen/Fuzzy+Bubbles", "Fuzzy Bubbles"],
-    ["https://fonts.google.com/specimen/Montserrat", "Montserrat"],
-    ["https://fonts.google.com/specimen/Roboto+Condensed", "Roboto Condensed"],
+    ["https://fonts.google.com/specimen/Fuzzy+Bubbles", "Fuzzy Bubbles", "fuzzy-bubbles-regular"],
+    ["https://fonts.google.com/specimen/Montserrat", "Montserrat", "montserrat-regular"],
+    ["https://fonts.google.com/specimen/Roboto+Condensed", "Roboto Condensed", "roboto-condensed-regular"],
   ];
 
   return (
@@ -13,18 +13,21 @@ export default function Footer() {
       <div>
         <h4>© 2025 - made with ❤️ by Sunny Huang</h4>
         <br />
-        <h4>fonts: </h4>
-        {fontLinks.map((link, index) => (
-          <motion.a
-            href={link[0]}
-            target="_blank"
-            rel="noopener noreferrer"
-            whileHover={{ scale: 1.1 }}
-            key={index}
-          >
-            <h4>- {link[1]}</h4>
-          </motion.a>
-        ))}
+        <h4>
+          fonts:{" "}
+          {fontLinks.map((link, index) => (
+            <motion.a
+              href={link[0]}
+              target="_blank"
+              rel="noopener noreferrer"
+              whileHover={{ scale: 1.1 }}
+              key={index}
+              className={link[2]}
+            >
+              {link[1]}{index < fontLinks.length - 1 ? ", " : ""}
+            </motion.a>
+          ))}
+        </h4>
       </div>
     </div>
   );
